04f46a41559d66ada84a1791e16da49c2cd3140b
Yujin Lee <yujinyll@naver.com>
2024-02-18 15:03:43 +0900

    [feat] stt 완성

--- lib/screens/learning/learning_screen.dart
+++ lib/screens/learning/learning_screen.dart
@@ -1,15 +1,12 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:google_speech/google_speech.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:lipread/models/message_model.dart';
 import 'package:lipread/screens/learning/widgets/controls_overlay.dart';
 import 'package:lipread/services/learning_service.dart';
 import 'package:lipread/utilities/app_color_scheme.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
-import 'package:record/record.dart';
 import 'widgets/message_card.dart';
 
 class LearningScreen extends StatefulWidget {
@@ -25,13 +22,10 @@ class LearningScreen extends StatefulWidget {
 }
 
 class _LearningScreenState extends State<LearningScreen> {
-  bool isRecording = false;
-  String? audioPath;
   int currentLearningMessageIndex = 0;
   bool _speechEnabled = false;
-  String _lastWords = '';
+  String _recognizedWords = '';
 
-  late AudioRecorder _audioRecorder;
   late stt.SpeechToText _speechToText;
   late VideoPlayerController _controller;
   late Future<List<MessageModel>> _messages;
@@ -43,7 +37,6 @@ class _LearningScreenState extends State<LearningScreen> {
     super.initState();
     _messages = LearningService.getMessagesBy(widget.id);
 
-    _audioRecorder = AudioRecorder();
     _speechToText = stt.SpeechToText();
 
     _initSpeech();
@@ -61,7 +54,6 @@ class _LearningScreenState extends State<LearningScreen> {
   @override
   void dispose() {
     super.dispose();
-    _audioRecorder.dispose();
     _controller.dispose();
   }
 
@@ -71,56 +63,22 @@ class _LearningScreenState extends State<LearningScreen> {
   }
 
   void _startListening() async {
+    debugPrint('Start Listening');
     await _speechToText.listen(onResult: _onSpeechResult);
     setState(() {});
   }
 
   void _stopListening() async {
+    debugPrint('Stop Listening');
     await _speechToText.stop();
     setState(() {});
   }
 
   void _onSpeechResult(SpeechRecognitionResult result) {
+    debugPrint('음성 결과: ${result.finalResult}, 단어: ${result.recognizedWords}');
     setState(() {
-      _lastWords = result.recognizedWords;
+      _recognizedWords = result.recognizedWords;
     });
-  }
-
-  Future<void> startRecording() async {
-    try {
-      if (await _audioRecorder.hasPermission()) {
-        String localPath = await _localPath;
-        debugPrint(localPath);
-        await _audioRecorder.start(
-          const RecordConfig(),
-          path: '$localPath/record.m4a',
-        );
-        setState(() {
-          isRecording = true;
-        });
-      }
-    } catch (e) {
-      debugPrint('Error Start Recording: $e');
-    }
-  }
-
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<void> stopRecording() async {
-    try {
-      String? path = await _audioRecorder.stop();
-      setState(() {
-        isRecording = false;
-        audioPath = path;
-      });
-      debugPrint(path!);
-    } catch (e) {
-      debugPrint('Error Stop Recording: $e');
-    }
   }
 
   @override
@@ -173,12 +131,8 @@ class _LearningScreenState extends State<LearningScreen> {
                                 itemBuilder: (context, index) {
                                   return MessageCard(
                                     id: messages[index].id,
-                                    role: _speechToText.isListening
-                                        ? _lastWords
-                                        : _speechEnabled
-                                            ? 'tap the micro'
-                                            : 'speech not available',
-                                    text: messages[index].text,
+                                    text: _recognizedWords,
+                                    role: messages[index].text,
                                     videoUrl: messages[index].videoUrl,
                                   );
                                 },
@@ -200,7 +154,7 @@ class _LearningScreenState extends State<LearningScreen> {
                           glowColor: AppColor.primaryColor,
                           duration: const Duration(milliseconds: 1200),
                           glowRadiusFactor: 0.3,
-                          animate: isRecording,
+                          animate: !_speechToText.isNotListening,
                           child: Container(
                             decoration: BoxDecoration(
                               color: AppColor.primaryColor,
@@ -214,9 +168,9 @@ class _LearningScreenState extends State<LearningScreen> {
                                 padding: const EdgeInsets.all(20),
                                 iconSize: 40,
                                 icon: Icon(
-                                  isRecording
-                                      ? Icons.multitrack_audio_rounded
-                                      : Icons.keyboard_voice_rounded,
+                                  _speechToText.isNotListening
+                                      ? Icons.keyboard_voice_rounded
+                                      : Icons.multitrack_audio_rounded,
                                 )),
                           ),
                         ),
